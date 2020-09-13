869aba919a9d7286a18d52f56bcd7f643140fb2b
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-13 20:58:37 +0700

    Thêm phần tử vào mảng

---
+++ ThemPhanTuVaoMang/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace ThemPhanTuVaoMang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int[] arr = { 7, 5, 9, 2, 1 };
+            int x, k;
+            Console.WriteLine("Nhập x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhập vị trí cần chèn:");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mảng trước khi chèn: ");
+            HienThi(arr, arr.Length - 1);
+            Chen(arr, x, k);
+            Console.WriteLine("Mảng sau khi chèn: ");
+            HienThi(arr, arr.Length);
+        }
+
+        private static void Chen(int[] arr, int x, int k)
+        {
+            for (int i = arr.Length - 1; i > k; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+            arr[k] = x;
+        }
+
+        static void HienThi(int[] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
